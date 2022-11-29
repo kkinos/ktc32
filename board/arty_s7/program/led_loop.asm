07a7665b6655bff611419ba9fe17c375008420bd
kinpoko <79001545+kinpoko@users.noreply.github.com>
2022-11-29 22:58:55 +0900

    Change clk 12MHz -> 100MHz

--- board/arty_s7/program/led_loop.asm
+++ board/arty_s7/program/led_loop.asm
@@ -1,6 +1,7 @@
 mov r1, r0
 lui r9, r9, -16
-addi r3, r0, 32767
+lui r3, r3, 16
+addi r3, r3, 32767
 
 mov r2, r0
 
@@ -14,4 +15,4 @@ jal r0, -12
 addi r4, r0, 16
 beq r1, r4, 4
 jal r0, -34
-jal r0, -48
+jal r0, -52
