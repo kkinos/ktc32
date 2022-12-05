daeb405b9feec83c752f195038d6cb3a76a65e2c
kinpoko <79001545+kinpoko@users.noreply.github.com>
2022-12-05 10:42:38 +0900

    Fix led_loop.asm

--- board/arty_s7/program/led_loop.asm
+++ board/arty_s7/program/led_loop.asm
@@ -1,18 +1,18 @@
-mov r1, r0
-lui r9, -16
-lui r3, 16
-addi r3, r3, 32767
+lui r1, -16
+lui r2, 16
+addi r2, r2, 32767
+addi r3, r0, 16
 
-mov r2, r0
+mov r4, r0
+mov r5, r0
 
-addi r1, r1, 1
-sw r1, r9, 0
+addi r4, r4, 1
+sw r4, r1, 0
 
-addi r2, r2, 1
-beq r2, r3, 4
+addi r5, r5, 1
+beq r5, r2, 4
 jal r0, -12
 
-addi r4, r0, 16
-beq r1, r4, 4
-jal r0, -34
-jal r0, -52
+beq r4, r3, 4
+jal r0, -30
+jal r0, -36
