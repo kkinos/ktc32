eae07cbdb63054d788b55997e50934b6fdd37b0c
kinpoko <79001545+kinpoko@users.noreply.github.com>
2023-01-07 01:05:32 +0900

    Move files and directories

--- program/sample.asm
+++ program/sample.asm
@@ -35,6 +35,10 @@ turn_on_led:
 	mov r5, r0
 
 wait:
+	lui r6, 0xfff1
+	lbu r7, r6, 0x8
+	beq r7, r0, 4
+	jal r0, start_echo_back
 	addi r5, r5, 1
 	beq r5, r2, 4
 	jal r0, wait
@@ -44,3 +48,32 @@ count_up_led:
 	bnq r4, r3, 2
 	mov r4, r0
 	jal r0, turn_on_led
+
+start_echo_back:
+	lui r1, 0xfff1
+	addi r2, r0, 1
+	addi r3, r0, 2
+
+loop:
+	jal r4, getc
+	jal r4, putc
+	jal r0, loop
+
+getc:
+	lbu r5, r1, 0x8
+	andi r5, r5, 2
+	beq r5, r3, 4
+	jal r0, load_zero
+	lbu r6, r1, 0xc
+	jalr r0, r4, 0
+
+load_zero:
+	mov r6, r0
+	jalr r0, r4, 0
+
+putc:
+	lbu r7, r1, 0
+	beq r7, r0, 4
+	jal r0, putc
+	sw r6, r1, 4
+	jalr r0, r4, 0
